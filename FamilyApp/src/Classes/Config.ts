a5179b4d64578b52b90ba2b94a11fa9cb564129f
翁志来MAC <wzl0313@hotmail.com>
2018-05-03 00:49:52 +0800

    BUG

--- FamilyApp/src/Classes/Config.ts
+++ FamilyApp/src/Classes/Config.ts
@@ -10,14 +10,14 @@ export class Config {
   static debug:boolean = true;
   static pageSize:number = 10;
 
-  static api:string = 'http://127.0.0.1:5000/Api/';
-  static imgUrl:string = 'http://127.0.0.1:5000/';
+  // static api:string = 'http://127.0.0.1:5000/Api/';
+  // static imgUrl:string = 'http://127.0.0.1:5000/';
 
   // static api:string = 'http://192.168.0.154:5000/Api/';
   // static imgUrl:string = 'http://192.168.0.154:5000/';
   static userType="user"
-  // static api:string = 'http://47.254.16.126:8080/Api/';
-  // static imgUrl:string = 'http://47.254.16.126:8080/';
+  static api:string = 'http://47.254.16.126:8080/Api/';
+  static imgUrl:string = 'http://47.254.16.126:8080/';
 
 
 
